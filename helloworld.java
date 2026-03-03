f5af95756c139e14575a5cac0a7a4fcb10844717
Deepak Repo <dpk.ewit@gmail.com>
2026-03-04 00:09:46 +0530

    Update trigger message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
         System.out.println("Hello, Deepak kumar Sinha in Git hub!");
-        System.out.println("triger, it's triger sucssfull once change commit done!");
+        System.out.println("triger, it's triger sucssfull once change done now!");
     }
 }
