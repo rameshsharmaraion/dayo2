c3a7e21c6f4cb767f9c47e2b1ff900afbdac40cf
rameshsharmaraion <150923070+rameshsharmaraion@users.noreply.github.com>
2026-04-08 20:31:12 +0530

    Update greeting and message in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 public class helloworld {
     public static void main(String[] args) {
-        System.out.println("Hello, Ramesh kumar in Git hub!");
-        System.out.println("pipeline, it's Ramesh Kumar sucssfull once change done 8th April!");
+        System.out.println("Hello, Ramesh kumar Sharma in Git hub!");
+        System.out.println("pipeline, it's Ramesh Kumar Sharma sucssfull once change done 8th April!");
     }
 }
